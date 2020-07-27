dbf28ff10763b1a6a1faf5a41a825971f55c5cad
vignesh77080 <vignesh.yes97@gmail.com>
2020-07-27 16:42:51 +0530

    Updates

---
+++ precache-manifest.ac1a69103365d51b5cba732b638d6e30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d6138d0d66e699753dd5eab726bebc8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "11b1dc111ce31c1080ff",
+    "url": "/portfolio/static/css/main.672d5783.chunk.css"
+  },
+  {
+    "revision": "3565f80a8df1854dd089",
+    "url": "/portfolio/static/js/2.785a1d06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.785a1d06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11b1dc111ce31c1080ff",
+    "url": "/portfolio/static/js/main.88b00014.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
